0ce1363c1bd5f1f8d2344dfc67119d3b0dfb2952
Denzel Jhones <denzelxd09@icloud.com>
2024-05-03 21:00:52 -0300

    alteração 1 valor de pi

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,Khorus");
-    }
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+        final double VALOR_DE_PI = 3.14;
+    
+         }
 }
