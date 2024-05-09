8644a42d2cfeac15b283c3d14057d50f00ee7f60
Denzel Jhones <denzelxd09@icloud.com>
2024-05-09 20:10:45 -0300

    condicionais

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -21,7 +21,7 @@
 		System.out.printf("Olá %s, obrigado por criar uma conta em "
 				+ "nosso banco, sua agência é %s, "
 				+ "conta %d e seu saldo %.2f já está disponível"
-				+ " para saque.", nomeCliente, agencia, numero, saldo);
+				+ " para saque.", nomeCliente,agencia, numero, saldo);
 
 		sc.close();
 }
